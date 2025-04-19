f333c213e38226380c0402fd1be45deee5bf3511
LYGreen <3098036603@qq.com>
2025-04-19 15:54:20 +0800

    ConditionalRendering

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import TemplateSyntax from './components/TemplateSyntax.vue';
 import PropertyBinding from './components/PropertyBinding.vue'
+import ConditionalRendering from './components/ConditionalRendering.vue'
 </script>
 
 <template>
     <!-- <TemplateSyntax/> -->
-    <PropertyBinding/>
+    <!-- <PropertyBinding/> -->
+    <ConditionalRendering/>
 </template>
 
 
---
+++ src/components/ConditionalRendering.vue
@@ -0,0 +1,23 @@
+<template>
+    <h3>Conditional Rendering</h3>
+    <div v-if="flag">flag is true</div>
+    <div v-else>flag is false</div>
+
+    <div v-if="type === 'A'">A type</div>
+    <div v-else-if="type === 'B'">A type</div>
+    <div v-else-if="type === 'C'">A type</div>
+    <div v-else>Not A/B/C</div>
+
+    <div v-show="flag">test v-show</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            flag: true,
+            type: 'D',
+        }
+    }
+}
+</script>
