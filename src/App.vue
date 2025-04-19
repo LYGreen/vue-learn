7ad0250eb44a5e12b994e0173205abca42c692fd
LYGreen <3098036603@qq.com>
2025-04-19 16:02:19 +0800

    ListRendering

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import TemplateSyntax from './components/TemplateSyntax.vue';
 import PropertyBinding from './components/PropertyBinding.vue'
 import ConditionalRendering from './components/ConditionalRendering.vue'
+import ListRendering from './components/ListRendering.vue';
 </script>
 
 <template>
     <!-- <TemplateSyntax/> -->
     <!-- <PropertyBinding/> -->
-    <ConditionalRendering/>
+    <!-- <ConditionalRendering/> -->
+    <ListRendering/>
 </template>
 
 
---
+++ src/components/ListRendering.vue
@@ -0,0 +1,22 @@
+<template>
+    <h3>List Rendering</h3>
+    <p v-for="item in items">{{ item }}</p>
+    <p v-for="item of items">{{ item }}</p>
+    <p v-for="(item, index) in items">{{ index }}: {{ item }}</p>
+    <p v-for="(item, key, index) in obj">{{ index }}. {{ key }}: {{ item }}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: ["apple", "pear", "watermalon"],
+            obj: {
+                id: 1,
+                name: "myname",
+                bio: "null"
+            }
+        }
+    }
+}
+</script>
